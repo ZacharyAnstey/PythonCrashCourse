082c9e12c756a38c79d3d32ea6c3a158bc698453
Zachary Anstey <77645692+ZacharyAnstey@users.noreply.github.com>
2023-03-29 11:05:31 -0230

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,67 @@
+"""
+String formating and methods
+"""
+
+name = 'Zachary'
+age = 25
+
+
+# concatenate
+# print('Hello, my name is ' + name +' an I am ' + str(age))
+
+# String formatting 
+
+# Positional arguments 
+# print('My name is{name} and I am {age}'.format(name=name,age=age))
+
+# F-strings 
+# print(f'Hello, my name is {name}, and I am {age}')
+
+# String methods
+
+s = 'hello world'
+
+# capitalize 
+# print(s.capitalize())
+
+# Make all uppercase 
+# print(s.upper())
+
+# Make all lower 
+# print(s.lower())
+
+
+# Swap case 
+# print(s.swapcase())
+
+# get length
+# print(len(s))
+
+
+# Replace 
+# print(s.replace('world','everyone'))
+
+# count 
+# sub = 'h'
+# print(s.count(sub))
+
+# Starts with 
+# print(s.startswith('hello'))
+
+# Ends with 
+# print(s.endswith('world'))
+
+# split into a list 
+# print(s.split())
+
+# Find position 
+# print(s.find('r'))
+
+# Is all alphanumeric 
+# print(s.isalnum())
+
+# Is all alphabetic 
+# print(s.isalpha())
+
+# Is all numeric 
+# print(s.isnumeric())
